a02a3606981fbe3f7f5ce1bd742282d3e9a244d7
egs125 <egs125@gmail.com>
2024-09-03 21:50:52 +0900

    엔티티 설정 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id
     @GeneratedValue
